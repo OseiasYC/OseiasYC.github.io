d7cf2e86e51a68c140369937d91c1416329d721a
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2022-10-29 00:44:45 -0300

    Overflow fixed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// function language() {
+
+//     let eng = {
+//             hi:"Hello, world! Nice,", 
+//             oseias:"Teste"
+
+//     }
+
+//     hi.innerText = eng.hi;
+//     oseias.innerText = eng.oseias;
+// }
+
+// language();
 AOS.init();
