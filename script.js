dcf8ca8cc744060c315182775e92a4347e215f48
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2022-10-30 22:17:59 -0300

    Code revision

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// function language() {
+var slides = document.querySelector(".slides");
+var slideSize = slides.children.length;
+var index = 0;
 
-//     let eng = {
-//             hi:"Hello, world! Nice,", 
-//             oseias:"Teste"
+//Slider buttons:
+function next() {
+    index++;
+    if (index >= slideSize) {
+        slides.scrollLeft -= slides*slideSize;
+        index = 0;
+    }else{
+        slides.scrollLeft += slides.clientWidth;
+    }
+    console.log(index);
+}
+function previous() {
+    index--;
+    if (index <= 0) {
+        slides.scrollLeft += slides.clientWidth*slideSize;
+        index = 0;
+    }else{
+        slides.scrollLeft -= slides.clientWidth;
+    }
+    console.log(index);
+}
 
-//     }
-
-//     hi.innerText = eng.hi;
-//     oseias.innerText = eng.oseias;
-// }
-
-// language();
+//Animate on scroll:
 AOS.init();
