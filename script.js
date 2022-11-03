a11d76b7f3589ed91d8657768ca50d9294628628
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2022-11-03 18:06:03 -0300

    Form section added

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //Slider buttons:
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
     direction: 'vertical',
     loop: true,
 
-  // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
 
-  // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
