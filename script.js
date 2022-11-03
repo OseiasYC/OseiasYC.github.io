2b8015adc8ba80221f1655ab8f68ea80fa728b8a
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2022-11-03 19:49:54 -0300

    Added responsivess navigation

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//Responsivess navigation
+function nav(){
+    var nav = document.querySelector("nav");
+    var checkBox = document.querySelector("#bars");
+
+    if (checkBox.checked == true) {
+        nav.style.visibility = "visible";
+        document.documentElement.style.overflow = 'hidden';
+    } else {
+        nav.style.visibility = "hidden";
+        document.documentElement.style.overflowY = 'auto';
+    }
+    nav.addEventListener('click', function() {
+        nav.style.visibility = "hidden";
+        checkBox.checked = false;
+        document.documentElement.style.overflowY = 'auto';
+    });
+    
+}
+
 //Slider buttons:
 const swiper = new Swiper('.swiper', {
     direction: 'vertical',
